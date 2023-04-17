a68e5dfc8b071793895c9fcd3bcfa0e2c70589a9
Miro Kropacek <miro.kropacek@gmail.com>
2023-04-18 01:09:06 +0200

    Blocking mainloop

--- nbuffvdi.c
+++ nbuffvdi.c
@@ -29,8 +29,7 @@ int main(int argc, char *argv[]) {
     }
 
     // work_in[0] = 1: current resolution; others: defaults
-    for(int i = 0; i < 10; i++)
-        work_in[i] = 1;
+    for(int i = 0; i < 10; work_in[i++] = 1);
     // raster coordinates
     work_in[10] = 2;
     // work_in[11] to work_in[15] are only useful with NVDI
@@ -49,8 +48,8 @@ int main(int argc, char *argv[]) {
         }
     } else {
         // get the ID of the current physical screen workstation
-        int16_t dummy;
-        vdi_handle = graf_handle(&dummy, &dummy, &dummy, &dummy);
+        int16_t wcell, hcell, wbox, hbox;
+        vdi_handle = graf_handle(&wcell, &hcell, &wbox, &hbox);
         if (vdi_handle < 1) {
             appl_exit();
 
@@ -70,16 +69,48 @@ int main(int argc, char *argv[]) {
             return EXIT_FAILURE;
         }
 
+        graf_mouse(M_OFF, NULL);
         wind_update(BEG_UPDATE);
     }
 
-    // TODO: code
-    printf("all good\r\n");
+    ///////////////////////////////////////////////////////////////////////////
+
+    const int width = work_out[0] + 1;
+    const int height = work_out[1] + 1;
+    printf("all good: %d x %d\r\n", width, height);
+
+    uint8_t ch = 0xff;
+    do {
+        //v_clrwk(vdi_handle);
+
+        static int col;
+
+        short pxy[4];
+        // color 0
+        vsf_color(vdi_handle, col);
+        vsf_interior(vdi_handle, FIS_SOLID);
+        vsf_perimeter(vdi_handle, PERIMETER_OFF);
+
+        pxy[0] = pxy[1] = 0;
+        pxy[2] = width - 1;
+        pxy[3] = height - 1;
+        v_bar(vdi_handle, pxy);
+
+        col = ~col & 0x01;
+
+        // loop until ESC = 0x01 is pressed (blocking)
+    } while ((aes_present && (ch = ((evnt_keybd() >> 8) & 0xff)) != 0x01)
+             || (!aes_present && (ch = ((Crawcin() >> 16) & 0xff)) != 0x01));
+
+    ///////////////////////////////////////////////////////////////////////////
 
     if (app_id != -1) {
-        v_clsvwk(vdi_handle);
-
         wind_update(END_UPDATE);
+        // redraw screen
+        form_dial(FMD_FINISH, 0, 0, 0, 0, 0, 0, width, height);
+        graf_mouse(M_ON, NULL);
+
+        v_clsvwk(vdi_handle);
         appl_exit();
     } else {
         v_clswk(vdi_handle);
