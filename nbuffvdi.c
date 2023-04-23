0010ee7a5120257ba08f2daa889a66de91d0939a
Miro Kropacek <miro.kropacek@gmail.com>
2023-04-23 22:05:31 +0200

    quick experiment with screenptr, definitely looking better

--- nbuffvdi.c
+++ nbuffvdi.c
@@ -18,16 +18,23 @@ enum {
     BUFFER_COUNT
 };
 
+struct Rect {
+    short x;
+    short y;
+    short w;
+    short h;
+};
+
 struct Screen {
     void* p;
     void* p_unaligned;
-    short pxy[4];
+    struct Rect rect;
 };
 
-static int width;
-static int height;
-static int bpp;
-static short pxy[4];
+static int width = 320;
+static int height = 240;
+static int bpp = 2;
+static struct Rect rect;
 static size_t screen_size;
 
 static struct Screen screen[BUFFER_COUNT];
@@ -42,10 +49,10 @@ static enum {
 } render_mode = DIRECT;
 
 void set_rect_min() {
-    pxy[0] = 0;
-    pxy[1] = height / 2 - (height / 8) / 2;
-    pxy[2] = (width / 8);
-    pxy[3] = height / 2 + (height / 8) / 2;
+    rect.x = 0;
+    rect.y = height / 2 - (height / 8) / 2;
+    rect.w = (width / 8);
+    rect.h = (height / 8);
 }
 
 static void reset_screens() {
@@ -53,10 +60,8 @@ static void reset_screens() {
         p_screen[i] = &screen[i];
 
         memset(screen[i].p, 0, width * height * bpp);
-        memset(screen[i].pxy, 0, sizeof(screen[i].pxy));
+        memset(&screen[i].rect, 0, sizeof(screen[i].rect));
     }
-
-    p_work_screen = p_screen[FRONT_BUFFER];
 
     set_rect_min();
 }
@@ -80,23 +85,6 @@ static void Vsync() {
 int main(int argc, char *argv[]) {
     int16_t app_id = appl_init();
     bool aes_present  = aes_global[0] != 0x0000;
-    int16_t vdi_handle;
-    int16_t work_in[16] = {};
-    int16_t work_out[57] = {};
-    bool is_falcon;
-
-    enum {
-        MCH_ST = 0,
-        MCH_STE,
-        MCH_TT,
-        MCH_FALCON,
-        MCH_CLONE,
-        MCH_ARANYM
-    };
-    long mch = MCH_ST<<16;
-    Getcookie(C__MCH, &mch);
-    mch >>= 16;
-    is_falcon = (mch == MCH_FALCON);
 
     if (app_id == -1 && aes_present) {
         fprintf(stderr, "appl_init() failed.\r\n");
@@ -104,62 +92,18 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // work_in[0] = 1: current resolution; others: defaults
-    for (int i = 0; i < 10; work_in[i++] = 1);
-    // raster coordinates
-    work_in[10] = 2;
-    // work_in[11] to work_in[15] are only useful with NVDI
-
     if (app_id == -1) {
         // if AES is not present, clean up
         appl_exit();
-
-        // open a physical workstation
-        v_opnwk(work_in, &vdi_handle, work_out);
-
-        if (vdi_handle == 0) {
-            fprintf(stderr, "v_opnwk() failed.\r\n");
-            getchar();
-            return EXIT_FAILURE;
-        }
 
         // flush keyboard
         while (Cconis() == -1)  {
             Cnecin();
         }
-    } else {
-        // get the ID of the current physical screen workstation
-        int16_t wcell, hcell, wbox, hbox;
-        vdi_handle = graf_handle(&wcell, &hcell, &wbox, &hbox);
-        if (vdi_handle < 1) {
-            appl_exit();
-
-            fprintf(stderr, "graf_handle() failed.\r\n");
-            getchar();
-            return EXIT_FAILURE;
-        }
-
-        // open a virtual screen workstation
-        v_opnvwk(work_in, &vdi_handle, work_out);
-
-        if (vdi_handle == 0) {
-            appl_exit();
-
-            fprintf(stderr, "v_opnvwk() failed.\r\n");
-            getchar();
-            return EXIT_FAILURE;
-        }
-
-        graf_mouse(M_OFF, NULL);
-        wind_update(BEG_UPDATE);
     }
 
     ///////////////////////////////////////////////////////////////////////////
 
-    width = work_out[0] + 1;
-    height = work_out[1] + 1;
-    // if >32767 then assume 32-bit else 8-bit, precise number doesn't really matter
-    bpp = work_out[39] == 0 ? 4 : 1;
     screen_size = width * height * bpp;
 
     for (int i = 0; i < BUFFER_COUNT; ++i) {
@@ -174,9 +118,6 @@ int main(int argc, char *argv[]) {
 
         screen[i].p = (void *)(((uintptr_t)screen[i].p_unaligned + ALIGN-1) & -ALIGN);
     }
-
-    reset_screens();
-    v_clrwk(vdi_handle);
 
     const EVMULT_IN evmult_in = {
         .emi_flags      = MU_KEYBD | MU_TIMER,
@@ -195,17 +136,11 @@ int main(int argc, char *argv[]) {
         .emi_thigh      = 0                 //
     };
 
-    {
-        // VDI setup
-        int16_t hor_out, vert_out;
-        vst_alignment(vdi_handle, TA_LEFT, TA_TOP, &hor_out, &vert_out);
-
-        vsf_interior(vdi_handle, FIS_HATCH);
-        vsf_perimeter(vdi_handle, PERIMETER_OFF);
-    }
-
     void *old_logbase = Logbase();
     void *old_physbase = Physbase();
+    short old_mode = VsetMode(VM_INQUIRE);
+
+    VsetMode(COL40|PAL|VGA|BPS16|VERTFLAG);
 
     int mul = 1;
     float pos = 0;
@@ -213,6 +148,11 @@ int main(int argc, char *argv[]) {
     bool quit = false;
     do {
         char ascii = -1;
+        static bool first_run = true;
+        if (first_run) {
+            ascii = '0';
+            first_run = false;
+        }
 
         if (aes_present) {
             int16_t msg_buffer[8];
@@ -226,42 +166,37 @@ int main(int argc, char *argv[]) {
                 ascii = Cnecin() & 0xFF;
         }
 
-        // as VDI draws into Logbase and we want to avoid using offscreen bitmaps
-        // (and NVDI), use it as our logical buffer
-
         switch (ascii) {
         case ' ':
             quit = true;
             break;
         case '0':
             render_mode = DIRECT;
-            // we're going to use pScreen[FRONT_BUFFER].pxy so call reset_screens() for init
             reset_screens();
             p_work_screen = p_screen[FRONT_BUFFER];
             Vsync();
-            Setscreen(old_logbase, old_physbase, SCR_NOCHANGE);
-            v_clrwk(vdi_handle);
+            VsetScreen(SCR_NOCHANGE, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
             break;
         case '1':
             render_mode = SINGLE;
             reset_screens();
             p_work_screen = p_screen[FRONT_BUFFER];
             Vsync();
-            Setscreen(p_screen[FRONT_BUFFER]->p, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE);
+            VsetScreen(SCR_NOCHANGE, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
             break;
         case '2':
             render_mode = DOUBLE;
             reset_screens();
             p_work_screen = p_screen[BACK_BUFFER1];
             Vsync();
-            Setscreen(p_screen[BACK_BUFFER1]->p, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE);
+            VsetScreen(SCR_NOCHANGE, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
             break;
         case '3':
             render_mode = TRIPLE;
             reset_screens();
             p_work_screen = p_screen[BACK_BUFFER1];
             Vsync();
-            Setscreen(p_screen[BACK_BUFFER1]->p, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE);
+            VsetScreen(SCR_NOCHANGE, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
         }
 
         if (ascii != -1) {
@@ -282,18 +217,20 @@ int main(int argc, char *argv[]) {
         old_ticks = curr_ticks;
 
         // clear screen
-        if (p_work_screen->pxy[2] != 0) {
-            vsf_color(vdi_handle, 0);
-            v_bar(vdi_handle, p_work_screen->pxy);
+        if (p_work_screen->rect.w != 0) {
+            char *p = (char *)p_work_screen->p + (p_work_screen->rect.y * width + p_work_screen->rect.x) * bpp;
+            for (int i = 0; i < p_work_screen->rect.h; ++i) {
+                memset(p, 0, p_work_screen->rect.w * bpp);
+                p += width * bpp;
+            }
         }
 
-        char str[256] = {0};
-        sprintf(str, "Buffers: %d, frame time (ms): %03ld, frames: %d       ", (int)render_mode, 1000 * diff / 200, frames);
-        v_gtext(vdi_handle, 0, 0, str);
-
-        vsf_color(vdi_handle, 1);
-        v_bar(vdi_handle, pxy);
-        memcpy(p_work_screen->pxy, pxy, sizeof(pxy));
+        char *p = (char *)p_work_screen->p + (rect.y * width + rect.x) * bpp;
+        for (int i = 0; i < rect.h; ++i) {
+            memset(p, 0xff, rect.w * bpp);
+            p += width * bpp;
+        }
+        memcpy(&p_work_screen->rect, &rect, sizeof(rect));
 
         /// end of drawing ///
 
@@ -301,11 +238,10 @@ int main(int argc, char *argv[]) {
         if (pos >= 1.0f) {
             pos = 0.0f;
 
-            pxy[0] += 1;
-            pxy[2] += 1;
+            rect.x += 1;
         }
 
-        if (pxy[2] >= width) {
+        if (rect.x + rect.w >= width) {
             mul++;
             if (mul >= 4) {
                 //mul = 1;
@@ -313,9 +249,9 @@ int main(int argc, char *argv[]) {
                 quit = true;
             } else {
                 set_rect_min();
-                pxy[1] -= 40*mul;
-                pxy[2] += 60*mul;
-                pxy[3] += 40*mul;
+                rect.y -= 20*mul;
+                rect.w += 60*mul;
+                rect.h += 40*mul;
             }
         }
 
@@ -329,17 +265,9 @@ int main(int argc, char *argv[]) {
             p_screen[FRONT_BUFFER] = p_screen[BACK_BUFFER1];
             p_screen[BACK_BUFFER1] = tmp;
             p_work_screen = p_screen[BACK_BUFFER1];
-            if (!is_falcon) {
-                // on ST/E, contrary to Falcon, reloading is happening on line 310 - 313;
-                // therefore if setting new video base address doesn't take more than 3 scan lines
-                // it will never lead to a corrupted screen address.
-                Setscreen(p_screen[BACK_BUFFER1]->p, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE);
-                Vsync();
-            } else {
-                Vsync();
-                // setting new video base address happens on line defined by VDB (after VBL)
-                VsetScreen(p_screen[BACK_BUFFER1]->p, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
-            }
+            Vsync();
+            // setting new video base address happens on line defined by VDB (after VBL)
+            VsetScreen(SCR_NOCHANGE, p_screen[FRONT_BUFFER]->p, SCR_NOCHANGE, SCR_NOCHANGE);
         } else if (render_mode == TRIPLE) {
             set_sysvar_to_short(vblsem, 0);  // lock vbl
 
@@ -365,7 +293,8 @@ int main(int argc, char *argv[]) {
 
             // queue BACK_BUFFER2 with the most recent frame content
             // (this will be set after VBL (Falcon) or before VBL (ST/STE/TT)
-            Setscreen(p_screen[BACK_BUFFER1]->p, p_screen[BACK_BUFFER2]->p, SCR_NOCHANGE);
+            //VsetScreen(SCR_NOCHANGE, p_screen[BACK_BUFFER2]->p, SCR_NOCHANGE, SCR_NOCHANGE);
+            set_sysvar_to_long(screenptr, p_screen[BACK_BUFFER2]->p);
 
             set_sysvar_to_short(vblsem, 1);  // unlock vbl
 
@@ -375,7 +304,8 @@ int main(int argc, char *argv[]) {
         frames++;
     } while (!quit);
 
-    Setscreen(old_logbase, old_physbase, SCR_NOCHANGE);
+    VsetMode(old_mode);
+    VsetScreen(old_logbase, old_physbase, SCR_NOCHANGE, SCR_NOCHANGE);
 
     for (int i = 0; i < BUFFER_COUNT; ++i) {
         Mfree(screen[i].p_unaligned);
@@ -385,15 +315,7 @@ int main(int argc, char *argv[]) {
     ///////////////////////////////////////////////////////////////////////////
 
     if (app_id != -1) {
-        wind_update(END_UPDATE);
-        // redraw screen
-        form_dial(FMD_FINISH, 0, 0, 0, 0, 0, 0, width, height);
-        graf_mouse(M_ON, NULL);
-
-        v_clsvwk(vdi_handle);
         appl_exit();
-    } else {
-        v_clswk(vdi_handle);
     }
 
     printf("Frames: %d\n", frames);
